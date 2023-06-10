e3edd18c9ad2fac6ba4b50aee0640761e4298a58
shoto-11 <107801320+shoto-11@users.noreply.github.com>
2023-06-10 20:07:43 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  plugins: [vue()],
+  build: {
+    rollupOptions: {
+      external: [
+        ".components/ToDoList.vue",
+        // 他のモジュールも追加する場合はここに追記する
+      ],
+    },
+  },
+});
+
