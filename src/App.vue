61158fed0f1300e5ee79e89ccc10d316c6dd37cb
shoto-11 <nisimiya0511@gmail.com>
2023-06-10 20:27:24 +0900

    これがtraP

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from ".components/ToDoList.vue";
+import HelloWorld from "./components/TodoList1.vue";
 </script>
 
 <template>
